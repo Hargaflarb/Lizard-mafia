e28d01b21d3428450bc9e1cbbce1134aa10298f6
Eleavate <echoovervad@gmail.com>
2025-04-18 14:03:23 +0200

    Basic Collision Functionality

--- Lizard game/Lizard game/ComponentPattern/Player.cs
+++ Lizard game/Lizard game/ComponentPattern/Player.cs
@@ -1,12 +1,11 @@
-﻿using Lizard_game.ComponentPattern;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lizard_game.Characters
+namespace Lizard_game.ComponentPattern
 {
     public class Player : Component
     {
@@ -20,8 +19,8 @@ namespace Lizard_game.Characters
         {
             SpriteRenderer sr = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
             sr.SetSprite("");
-            GameObject.Transform.Position = new Vector2(GameWorld.Instance.Graphics.PreferredBackBufferWidth / 2, 
-                (GameWorld.Instance.Graphics.PreferredBackBufferHeight - sr.Sprite.Height / 3) - 200);
+            GameObject.Transform.Position = new Vector2(GameWorld.Instance.Graphics.PreferredBackBufferWidth / 2,
+                GameWorld.Instance.Graphics.PreferredBackBufferHeight - sr.Sprite.Height / 3 - 200);
             speed = 300;
         }
 
@@ -37,7 +36,7 @@ namespace Lizard_game.Characters
 
         public override void Update()
         {
-            
+
         }
     }
 }
---
+++ Lizard game/Lizard game/ComponentPattern/Wall.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lizard_game.ComponentPattern
+{
+    public class Wall : Component
+    {
+        public Wall(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+        }
+        public override void Start()
+        {
+            base.Start();
+        }
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
