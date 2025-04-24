e5c6c38c5901378ea114bfb8fdaeee551b0f8d97
Hvemvilhvad <malthewbirch@gmail.com>
2025-04-24 10:22:13 +0200

    first commit

---
+++ Lizard game/Lizard game/ComponentPattern/Gravity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lizard_game.ComponentPattern
+{
+    public class Gravity : Component
+    {
+        private const float gravitation = 50;
+        private bool touching;
+        private float verticalVelocity;
+
+        public Gravity(GameObject gameObject) : base(gameObject)
+        {
+
+        }
+
+
+        public override void Update()
+        {
+            if (!touching)
+            {
+                verticalVelocity += gravitation;
+            }
+            else
+            {
+                verticalVelocity = 0;
+            }
+        }
+    }
+}
